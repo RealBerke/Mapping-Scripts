fca18bfbc3d0dbf38b71b87d057b36646bcf3db2
Berke <51463005+RealBerke@users.noreply.github.com>
2024-12-01 23:32:54 +0300

    Rewrite

--- Compile Pal/Parameters/SETMODELSUPPORT/setmodelsupport.py
+++ Compile Pal/Parameters/SETMODELSUPPORT/setmodelsupport.py
@@ -20,303 +20,299 @@ def Main():
 
 	iArgumentAmount = len(strArguments)
 
-	if iArgumentAmount > 1:
-		strMapPath = strArguments[1]
+	if iArgumentAmount == 1:
+		print("Path to the map \"" + strFileExtension.upper() + "\" file has to be supplied as the first argument.")
 
-		from os.path import isfile
+		return;
 
-		if isfile(strMapPath):
-			if strMapPath.endswith("." + strFileExtension):
-				if iArgumentAmount > 2:
-					strGameFolder = strArguments[2]
+	strMapPath = strArguments[1]
 
-					from os.path import isdir
+	from os.path import isfile
 
-					if isdir(strGameFolder):
-						if iArgumentAmount > 3:
-							strBSPZipPath = strArguments[3]
+	if not isfile(strMapPath):
+		print("First argument is not pointing to a file.")
 
-							if isfile(strBSPZipPath):
-								if iArgumentAmount > 4:
-									strPropDataPath = strArguments[4]
+		return
 
-									if isfile(strPropDataPath):
-										iFileType = 1
+	if not strMapPath.endswith("." + strFileExtension):
+		print("First argument is not pointing to a \"" + strFileExtension.upper() + "\" file.")
 
-										if strPropDataPath.endswith(".vpk"):
-											iFileType = 0
+		return
 
-											bIsValid = True
+	if iArgumentAmount == 2:
+		print("Path to the game folder has to be supplied as the second argument.")
 
-											strInternalFullPropDataPath = strInternalPropDataPath + strInternalPropDataFileName
+		return
 
-											import vpk
+	strGameFolder = strArguments[2]
 
-											try:
-												vpk.open(strPropDataPath)
+	from os.path import isdir
 
-											except:
-												bIsValid = False
+	if not isdir(strGameFolder):
+		print("Second argument is not pointing to a folder.")
 
-												print("Could not open the \"VPK\" file.")
+		return
 
-											if bIsValid:
-												VPK = vpk.open(strPropDataPath)
+	if iArgumentAmount == 3:
+		print("Path to the \"BSPZip\" file has to be supplied as the third argument.")
 
-												bIsValid = True
+		return
 
-												try:
-													VPK.get_file(strInternalFullPropDataPath)
+	strBSPZipPath = strArguments[3]
 
-												except:
-													bIsValid = False
+	if not isfile(strBSPZipPath):
+		print("Third argument is not pointing to a file.")
 
-													print("Could not find the \"" + strInternalPropDataFileName + "\" inside the \"VPK\" file.")
+		return
 
-												if bIsValid:
-													iFileType = 2
+	if not strBSPZipPath.endswith(".exe"):
+		print("Third argument is not pointing to a \"EXE\" file.")
 
-										if iFileType:
-											print("Modifying the \"" + strMapPath + "\" file.")
+		return
 
-											from bsp_tool import load_bsp
+	if iArgumentAmount == 4:
+		print("Path to the \"" + strInternalPropDataFileName + "\" file has to be supplied as the forth argument. Can be a \"VPK\" file.")
+		
+		return
 
-											BSP = load_bsp(strMapPath)
+	strPropDataPath = strArguments[4]
 
-											iFoundInputs = 0
+	if not isfile(strPropDataPath):
+		print("Forth argument is not pointing to a file.")
 
-											lModels = list()
+		return
 
-											iCacheAmount = iCacheStartIndex + len(BSP.MODELS)
+	bIsVPK = strPropDataPath.endswith(".vpk")
 
-											for dEntity in BSP.ENTITIES:
-												for strKeyValue in dEntity:
-													Value = dEntity[strKeyValue]
+	if not (strPropDataPath.endswith(".txt") or bIsVPK):
+		print("Forth argument is not pointing to a \"TXT\" or \"VPK\" file.")
 
-													if isinstance(Value, list):
-														for iOutputIndex, strOutput in enumerate(Value):
-															lResults = ModifyOutput(strOutput, lModels, iCacheAmount)
+		return
 
-															bFoundInput = lResults[0]
+	strInternalFullPropDataPath = strInternalPropDataPath + strInternalPropDataFileName
 
-															if bFoundInput:
-																iFoundInputs += bFoundInput
+	if bIsVPK:
+		import vpk
 
-																dEntity[strKeyValue][iOutputIndex] = lResults[1]
+		try:
+			vpk.open(strPropDataPath)
 
-																strModel = lResults[2]
+		except:
+			print("Could not open the \"VPK\" file.")
+			
+			return
 
-																if strModel not in lModels:
-																	lModels.append(strModel)
+		VPK = vpk.open(strPropDataPath)
 
-													else:
-														lResults = ModifyOutput(Value, lModels, iCacheAmount)
+		try:
+			VPK.get_file(strInternalFullPropDataPath)
 
-														bFoundInput = lResults[0]
+		except:
+			print("Could not find the \"" + strInternalPropDataFileName + "\" inside the \"VPK\" file.")
 
-														if bFoundInput:
-															iFoundInputs += bFoundInput
+			return
 
-															dEntity[strKeyValue] = lResults[1]
+	print("Modifying the \"" + strMapPath + "\" file.")
 
-															strModel = lResults[2]
+	from bsp_tool import load_bsp
 
-															if strModel not in lModels:
-																lModels.append(strModel)
+	BSP = load_bsp(strMapPath)
 
-											if iFoundInputs:
-												print("Modified " + str(iFoundInputs) + " \"" + strInputName + "\" input" + GetPluralitySuffix(iFoundInputs) + ".")
+	iFoundInputs = 0
 
-												BSP.save()
+	lModels = list()
 
-												lPacks = list()
+	iCacheAmount = iCacheStartIndex + len(BSP.MODELS)
 
-												strFixedGameFolder = strGameFolder.replace("\\", "/")
+	for dEntity in BSP.ENTITIES:
+		for strKeyValue in dEntity:
+			Value = dEntity[strKeyValue]
 
-												for strModel in lModels:
-													strModelPath = strFixedGameFolder + "/" + strModel
+			if isinstance(Value, list):
+				for iOutputIndex, strOutput in enumerate(Value):
+					lResults = ModifyOutput(strOutput, lModels, iCacheAmount)
 
-													if isfile(strModelPath):
-														lPacks.append(strModelPath)
+					if lResults[0]:
+						iFoundInputs += 1
 
-												iPackLength = len(lPacks)
+						dEntity[strKeyValue][iOutputIndex] = lResults[1]
 
-												if iPackLength:
-													if iPackLength == 1:
-														print("Pack this model:")
+						strModel = lResults[2]
 
-													else:
-														print("Pack these models:")
-
-													for strPack in lPacks:
-														print(strPack)
-
-												if iFileType == 1:
-													fPropData = open(strPropDataPath)
-
-												else:
-													from io import StringIO
-
-													fPropData = StringIO(vpk.open(strPropDataPath).get_file(strInternalFullPropDataPath).read().decode("utf-8"))
-
-												import vdf
-												from vdf import VDFDict
-
-												dPropData = vdf.load(fPropData)
-
-												if iFileType == 1:
-													fPropData.close()
-
-												if len(dPropData):
-													for strFirstKey in list(dPropData):
-														if strFirstKey == "PropData.txt" or strFirstKey == "0":
-															dPropDataFirst = dPropData[strFirstKey]
-
-															if len(dPropDataFirst):
-																if strFirstKey != "0":
-																	dPropData["0"] = dPropDataFirst
-
-																for strSecondKey in list(dPropDataFirst):
-																	if strSecondKey == strPropDataSection:
-																		dPropDataSecond = dPropDataFirst[strSecondKey]
-
-																		for strThirdKey in list(dPropDataSecond):
-																			dPropDataThird = dPropDataSecond[strThirdKey]
-
-																			for strForthKey in list(dPropDataThird):
-																				if not strForthKey.islower() or "\\" in strForthKey:
-																					del dPropDataThird[strForthKey]
-
-																					strForthKey = strForthKey.lower().replace("\\", "/")
-
-																					dPropDataThird[strForthKey] = 0
-
-																	else:
-																		del dPropData[strFirstKey][strSecondKey]
-
-															else:
-																dPropDataFirst[strPropDataSection] = VDFDict([("0", VDFDict())])
-
-														if strFirstKey != "0":
-															del dPropData[strFirstKey]
-
-												else:
-													dPropData["0"] = VDFDict([(strPropDataSection, VDFDict([("0", VDFDict())]))])
-
-												dSavedValues = dict(dPropData["0"][strPropDataSection])
-
-												dPropData["0"][strPropDataSection] = VDFDict([("0", VDFDict())])
-
-												for strModel in lModels:
-													dPropData["0"][strPropDataSection]["0"][strModel] = 0
-
-												for strKey, dValue in dSavedValues.items():
-													dPropData["0"][strPropDataSection][strKey] = dValue
-
-												strTemporaryPath = strBSPZipPath[0: strBSPZipPath.rfind("\\") + 1] + "Temp"
-
-												fNewFile = open(strTemporaryPath, "w")
-
-												fNewFile.write(vdf.dumps(dPropData, True))
-
-												fNewFile.close()
-
-												PackFile(strTemporaryPath, strInternalFullPropDataPath)
-
-												import os
-
-												os.remove(strTemporaryPath)
-
-											else:
-												print("Could not find any \"" + strInputName + "\" inputs.")
-
-											print("Finished!")
-
-									else:
-										print("Forth argument is not pointing to a file.")
-
-								else:
-									print("Path to the \"" + strInternalPropDataFileName + "\" file has to be supplied as the forth argument. Can be a \"VPK\" file.")
-
-							else:
-								print("Third argument is not pointing to a file.")
-
-						else:
-							print("Path to the \"BSPZip\" file has to be supplied as the third argument. Can be a \"VPK\" file.")
-
-					else:
-						print("Second argument is not pointing to a folder.")
-
-				else:
-					print("Path to the game folder has to be supplied as the second argument.")
+						if strModel not in lModels:
+							lModels.append(strModel)
 
 			else:
-				print("First argument is not pointing to a \"" + upper(strFileExtension) + "\" file.")
+				lResults = ModifyOutput(Value, lModels, iCacheAmount)
 
-		else:
-			print("First argument is not pointing to a file.")
+				if lResults[0]:
+					iFoundInputs += 1
+
+					dEntity[strKeyValue] = lResults[1]
+
+					strModel = lResults[2]
+
+					if strModel not in lModels:
+						lModels.append(strModel)
+
+	if not iFoundInputs:
+		print("Could not find any \"" + strInputName + "\" inputs.")
 
 	else:
-		print("Path to the map \"" + upper(strFileExtension) + "\" file has to be supplied as the first argument.")
+		print("Modified " + str(iFoundInputs) + " \"" + strInputName + "\" input" + GetPluralitySuffix(iFoundInputs) + ".")
+
+		BSP.save()
+
+		lPacks = list()
+
+		strFixedGameFolder = strGameFolder.replace("\\", "/")
+
+		for strModel in lModels:
+			strModelPath = strFixedGameFolder + "/" + strModel
+
+			if isfile(strModelPath):
+				lPacks.append(strModelPath)
+
+		iPackLength = len(lPacks)
+
+		if iPackLength:
+			print("Pack th" + ("is" if iPackLength == 1 else "ese") + ":")
+
+			for strPack in lPacks:
+				print(strPack)
+
+		if bIsVPK:
+			from io import StringIO
+
+			fPropData = StringIO(vpk.open(strPropDataPath).get_file(strInternalFullPropDataPath).read().decode("utf-8"))
+													
+		else:
+			fPropData = open(strPropDataPath)
+
+		import vdf
+		from vdf import VDFDict
+
+		dPropData = vdf.load(fPropData)
+
+		if not bIsVPK:
+			fPropData.close()
+
+		if not len(dPropData):
+			dPropData["0"] = VDFDict([(strPropDataSection, VDFDict([("0", VDFDict())]))])
+
+		else:
+			for strFirstKey in list(dPropData):
+				bIsFirstKeyNonZero = strFirstKey != "0"
+
+				if strFirstKey != "PropData.txt" and bIsFirstKeyNonZero:
+					del dPropData[strFirstKey]
+
+				else:
+					dPropDataFirst = dPropData[strFirstKey]
+
+					if bIsFirstKeyNonZero:
+						dPropData["0"] = dPropDataFirst
+
+						del dPropData[strFirstKey]
+
+						strFirstKey = "0"
+
+					if not len(dPropDataFirst):
+						dPropDataFirst[strPropDataSection] = VDFDict([("0", VDFDict())])
+
+					else:
+						for strSecondKey in list(dPropDataFirst):
+							if strSecondKey != strPropDataSection:
+								del dPropData[strFirstKey][strSecondKey]
+
+							else:
+								dPropDataSecond = dPropDataFirst[strSecondKey]
+
+								for strThirdKey in list(dPropDataSecond):
+									dPropDataThird = dPropDataSecond[strThirdKey]
+
+									for strForthKey in list(dPropDataThird):
+										if not strForthKey.islower() or "\\" in strForthKey:
+											del dPropDataThird[strForthKey]
+
+											strFixedForthKey = strForthKey.lower().replace("\\", "/")
+
+											dPropDataThird[strFixedForthKey] = 0
+
+		dSavedValues = dict(dPropData["0"][strPropDataSection])
+
+		dPropData["0"][strPropDataSection] = VDFDict([("0", VDFDict())])
+
+		for strModel in lModels:
+			dPropData["0"][strPropDataSection]["0"][strModel] = 0
+
+		for strKey, dValue in dSavedValues.items():
+			dPropData["0"][strPropDataSection][strKey] = dValue
+
+		strTemporaryPath = strBSPZipPath[0: strBSPZipPath.rfind("\\") + 1] + "Temp"
+
+		fNewFile = open(strTemporaryPath, "w")
+
+		fNewFile.write(vdf.dumps(dPropData, True))
+
+		fNewFile.close()
+
+		PackFile(strTemporaryPath, strInternalFullPropDataPath)
+
+		import os
+
+		os.remove(strTemporaryPath)
+
+	print("Finished!")
 
 def GetPluralitySuffix(iNumber):
 	return "" if iNumber == 1 else "s"
 
 def ModifyOutput(strOutput, lModels, iCacheAmount):
-	strModel = ""
+	if strOutput.count(strInputSeperator) != 4:
+		return [False]
 
-	bFoundInput = False
+	lOutput = strOutput.split(strInputSeperator)
 
-	if strOutput.count(strInputSeperator) == 4:
-		lOutput = strOutput.split(strInputSeperator)
+	if lOutput[1] != strInputName:
+		return [False]
 
-		try:
-			float(lOutput[1])
+	strThirdParameter = lOutput[2]
 
-		except:
-			strThirdParameter = lOutput[2]
+	if not (strThirdParameter.startswith("models/") and strThirdParameter.endswith(".mdl")):
+		return [False]
 
-			try:
-				float(strThirdParameter)
+	try:
+		float(lOutput[3])
 
-			except:
-				if strThirdParameter.startswith("models/") and strThirdParameter.endswith(".mdl"):
-					bIsValid = False
+	except:
+		return [False]
 
-					try:
-						float(lOutput[3])
+	strFifthParameter = lOutput[4]
 
-						bIsValid = True
+	if strFifthParameter != "-1" and not strFifthParameter.isdigit():
+		return [False]
 
-					except:
-						False
+	lOutput[1] = strWorkAroundInput
 
-					if bIsValid:
-						strFifthParameter = lOutput[4]
+	strModel = lOutput[2]
 
-						if strFifthParameter == "-1" or strFifthParameter.isdigit() and lOutput[1] == strInputName:
-							bFoundInput = True
+	iModelIndex = iCacheAmount
 
-							strModel = lOutput[2]
+	if strModel in lModels:
+		iModelIndex += lModels.index(strModel)
 
-							lOutput[1] = strWorkAroundInput
+	else:
+		iModelIndex += len(lModels)
 
-							iModelIndex = iCacheAmount
+	lOutput[2] = "modelindex " + str(iModelIndex)
 
-							if strModel in lModels:
-								iModelIndex += lModels.index(strModel)
+	strOutput = ""
 
-							else:
-								iModelIndex += len(lModels)
+	for iOutputIndex in range(len(lOutput)):
+		strOutput += lOutput[iOutputIndex] + ("" if iOutputIndex == 4 else strInputSeperator)
 
-							lOutput[2] = "modelindex " + str(iModelIndex)
-
-							strOutput = ""
-
-							for iOutputIndex in range(len(lOutput)):
-								strOutput += lOutput[iOutputIndex] + ("" if iOutputIndex == 4 else strInputSeperator)
-
-	return bFoundInput, strOutput, strModel
+	return True, strOutput, strModel
 
 def PackFile(strPath, strInternalPath):
 	from sys import argv
